ba3451493d8324c42b74d787ba6b692a0d550353
Kris <kristinaovchinnikova22@gmail.com>
2025-01-20 10:28:22 +0600

    HW#86 = made a Track History Card and Track history Page and get request by Track History

--- routers/trackHistories.ts
+++ routers/trackHistories.ts
@@ -1,40 +1,22 @@
 import express from "express";
-import User from "../models/User";
 import Track from "../models/Track";
 import TrackHistory from "../models/TrackHistory";
+import auth, {RequestWithUser} from "../middleware/auth";
 
 const trackHistoriesRouter = express.Router();
 
-trackHistoriesRouter.post('/', async (req, res, next) => {
+trackHistoriesRouter.post('/', auth, async (req, res, next) => {
+    let expressReq = req as RequestWithUser;
+    const user = expressReq.user;
+
+    if (!user) {
+        res.status(400).send({error: 'User not found'});
+        return;
+    }
   try{
-
-      const token = req.get('Authorization');
-
-      if(!token) {
-          res.status(401).send({error: 'No token present'});
-      }
-
-      const user = await User.findOne({token});
-
-      if(!user) {
-          res.status(401).send({error: 'Wrong token'});
-      }
-
-      const track = req.body.track;
-
-      if(!track){
-          res.status(400).send({error: 'Track ID is required'});
-      }
-
-      const trackById = await Track.findById(track);
-
-      if(!trackById){
-          res.status(400).send({error: 'Track not found'});
-      }
-
       const trackHistory = new TrackHistory({
-          user,
-          track,
+          user: user._id,
+          track: req.body.track,
           datetime: new Date(),
       })
       await trackHistory.save()
@@ -42,6 +24,60 @@ trackHistoriesRouter.post('/', async (req, res, next) => {
   }catch(error){
       next(error)
   }
+});
+
+trackHistoriesRouter.get('/', auth, async (req, res, next) => {
+    let expressReq = req as RequestWithUser;
+    const user = expressReq.user;
+
+    if (!user) {
+        res.status(400).send({error: 'User not found'});
+        return;
+    }
+
+    try{
+
+        const trackId = req.query.track;
+        let track;
+
+        if (trackId) {
+            track = await Track.findOne({ _id: trackId });
+            if (!track) {
+                res.status(404).send({ error: 'Track not found' });
+                return;
+            }
+        }
+
+        let trackHistory;
+
+        if (track) {
+            trackHistory = await TrackHistory.find({
+                user: user._id,
+                track: track._id,
+            });
+        } else {
+            trackHistory = await TrackHistory.find({ user: user._id })
+                .populate({
+                    path: 'track',
+                    populate: {
+                        path: 'album',
+                        model: 'Album',
+                        populate: {
+                            path: 'artist',
+                            model: 'Artist',
+                        },
+                    },
+                })
+                .sort({ datetime: -1 });
+        }
+
+        res.send(trackHistory);
+
+    }catch(e){
+        next(e);
+    }
 })
+
+
 
 export default trackHistoriesRouter;
